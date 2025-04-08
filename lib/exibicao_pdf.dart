5d2336297935b0ccb2f700663452d0a25507e9e5
samydsousa <samydsousa@hotmail.com>
2025-04-08 16:22:56 -0300

    Alteração do pacote para a visualização de PDF

--- lib/exibicao_pdf.dart
+++ lib/exibicao_pdf.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
 import 'dart:io';
-import 'package:advance_pdf_viewer_fork/advance_pdf_viewer_fork.dart';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:itens_do_enem/armazenamento.dart';
 import 'package:itens_do_enem/item.dart';
 
@@ -9,30 +13,28 @@ import 'excecoes.dart';
 class ExibicaoPDF extends StatefulWidget {
   final Item item;
 
-  ExibicaoPDF({
-    super.key,
-    required this.item,
-  });
+  ExibicaoPDF({Key? key, required this.item}) : super(key: key);
 
-  @override
   _ExibicaoPDFState createState() => _ExibicaoPDFState();
 }
 
 class _ExibicaoPDFState extends State<ExibicaoPDF> {
-  PDFDocument? _doc;
+  final Completer<PDFViewController> _controller =
+      Completer<PDFViewController>();
+  int? pages = 0;
+  late int? currentPage = widget.item.numPageCaderno - 1;
+  bool isReady = false;
+  String errorMessage = '';
+  File? _doc;
   late _ControleAnimacaoCarregamento _controleAnimacaoCarregamento;
   _ControleAnimacaoBanner _controleAnimacaoBanner = _ControleAnimacaoBanner();
-  PageController? _pageController;
 
-  Future<PDFDocument?> _getProva(int idProva) async {
+  Future<File?> _getProva(int idProva) async {
     File? file;
     try {
       //Usar arquivo no armazenamento
       file = await Armazenamento.getProva(idProva);
-      if (file == null) {
-        return null;
-      }
-      return await PDFDocument.fromFile(file);
+      return file;
     }
     //Ocorrido em getDirProvas()
     on AcessoAoArmazenamentoNegadoPermanentemente {
@@ -40,25 +42,18 @@ class _ExibicaoPDFState extends State<ExibicaoPDF> {
           "Se a permissão permanecer como negada as provas serão carregadas "
           "mais lentamente.";
       Armazenamento.abrirConfiguracoesApp(context, msg);
-      return _getProvaFromAsset(idProva);
+      return null;
     } catch (e) {
       //Caso não seja possível usar o arquivo no armazenamento
-      return _getProvaFromAsset(idProva);
+      return null;
     }
-  }
-
-  static Future<PDFDocument> _getProvaFromAsset(int idProva) async {
-    return await PDFDocument.fromAsset(
-        "${Armazenamento.ASSETS_PROVAS}$idProva.pdf");
   }
 
   @override
   void initState() {
     _controleAnimacaoCarregamento = _ControleAnimacaoCarregamento(context);
-    _pageController =
-        PageController(initialPage: widget.item.numPageCaderno - 1);
-    _getProva(widget.item.idProva).then((_) {
-      _doc = _;
+    _getProva(widget.item.idProva).then((file) {
+      _doc = file;
       _controleAnimacaoCarregamento.state = true;
     }).catchError((erro) {
       if (!_controleAnimacaoCarregamento.ocorreuErro)
@@ -70,32 +65,85 @@ class _ExibicaoPDFState extends State<ExibicaoPDF> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Item ${widget.item.idOrdem}  -  ${widget.item.gabarito == "X" ? "ANULADO" : "Gabarito " + widget.item.gabarito}",
-          ),
+      appBar: AppBar(
+        title: Text(
+          "Item ${widget.item.idOrdem}  -  ${widget.item.gabarito == "X" ? "ANULADO" : "Gabarito " + widget.item.gabarito}",
         ),
-        body: Center(
-            child: AnimatedBuilder(
-                animation: _controleAnimacaoCarregamento,
-                builder: (context, widget) {
-                  return AnimatedContainer(
-                      child: _doc == null
-                          ? const CircularProgressIndicator()
-                          : Column(children: [
-                              Expanded(
-                                  child: Stack(children: [
-                                PDFViewer(
-                                  showPicker: false,
-                                  document: _doc!,
-                                  controller: _pageController,
-                                ),
-                                //Criar banner de aviso
-                                _gerarBanner()
-                              ]))
-                            ]),
-                      duration: const Duration(milliseconds: 300));
-                })));
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controleAnimacaoCarregamento,
+          builder: (context, widget) {
+            return AnimatedContainer(
+                child: _doc == null
+                    ? const CircularProgressIndicator()
+                    : _buildPDFView(),
+                duration: const Duration(milliseconds: 300));
+          },
+        ),
+      ),
+    );
+  }
+
+  Column _buildPDFView() {
+    return Column(
+      children: [
+        Expanded(
+          child: Stack(
+            children: [
+              PDFView(
+                filePath: _doc!.path,
+                enableSwipe: true,
+                swipeHorizontal: true,
+                autoSpacing: false,
+                pageFling: true,
+                pageSnap: true,
+                defaultPage: currentPage!,
+                fitPolicy: FitPolicy.BOTH,
+                preventLinkNavigation:
+                    false, // if set to true the link is handled in flutter
+                onRender: (_pages) {
+                  setState(() {
+                    pages = _pages;
+                    isReady = true;
+                  });
+                },
+                onError: (error) {
+                  setState(() {
+                    errorMessage = error.toString();
+                  });
+                  print(error.toString());
+                },
+                onPageError: (page, error) {
+                  setState(() {
+                    errorMessage = '$page: ${error.toString()}';
+                  });
+                  print('$page: ${error.toString()}');
+                },
+                onViewCreated: (PDFViewController pdfViewController) {
+                  _controller.complete(pdfViewController);
+                },
+                onLinkHandler: (String? uri) {
+                  print('goto uri: $uri');
+                },
+                onPageChanged: (int? page, int? total) {
+                  print('page change: ${page ?? 0 + 1}/$total');
+                  setState(() {
+                    currentPage = page;
+                  });
+                },
+              ),
+
+              //Criar banner de aviso
+              _gerarBanner(),
+              Center(
+                child: Text(errorMessage), //TODO: Melhorar o widget da mensagem de erro
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 
   Widget _gerarBanner() {
